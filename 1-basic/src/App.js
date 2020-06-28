eb41977c675e046e1322ebfe58a50cf7e2230098
Mario Rizki <mariorizki07@gmail.com>
2020-06-29 02:14:53 +0700

    learn props

---
+++ 1-basic/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './components/Person';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm React App</h1>
+        <p>This is really working!</p>
+        <Person name="Max" age={28} />
+        <Person name="Manu" age={31} />
+        <Person name="Maudy" age={26} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ 1-basic/src/components/Person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Person = props => {
+  return (
+    <p>
+      I'm {props.name}, and I'm {props.age} years old!
+    </p>
+  );
+};
+
+export default Person;
