335a07eb4fdc02ac4e89248163dac27ab86fccc8
Mario Rizki <mariorizki07@gmail.com>
2020-06-29 02:37:48 +0700

    implement destructuring and state

--- 1-basic/src/App.js
+++ 1-basic/src/App.js
@@ -3,20 +3,25 @@ import './App.css';
 import Person from './components/Person';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+  state = {
+    person: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 31 },
+      { name: 'Maudy', age: 26 }
+    ]
+  };
   render() {
+    const { person } = this.state;
+
     return (
       <div className="App">
         <h1>Hi, I'm React App</h1>
         <p>This is really working!</p>
-        <Person name="Max" age={28} />
-        <Person name="Manu" age={31}>
+        <Person name={person[0].name} age={person[0].age} />
+        <Person name={person[1].name} age={person[1].age}>
           My hobbies: Reading
         </Person>
-        <Person name="Maudy" age={26} />
+        <Person name={person[2].name} age={person[2].age} />
       </div>
     );
   }
